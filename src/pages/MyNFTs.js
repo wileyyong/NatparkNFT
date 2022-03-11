d5c7ae8782f4d6513e7f2ce5cb9e660b2b1987f2
truelifedev <truelife1228@outlook.com>
2022-03-11 10:48:26 -0500

    added park filter

--- src/pages/MyNFTs.js
+++ src/pages/MyNFTs.js
@@ -23,7 +23,9 @@ function MyNFTs() {
 			const data = result?.map(item => ({
 				...item, 
 				metadata: JSON.parse(item.metadata)
-			})).filter(item => item.metadata);
+			}))
+			.filter(item => item.metadata)
+			.filter(item => item.token_address === "0x64dc7f3624a1456a7ba52025fcfddf428fff92e0");
 			setNFTs(data);
 		});
 	}, []);
