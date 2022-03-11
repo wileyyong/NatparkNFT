0633c2b0ce38073d5d3b84b2cf49b31c1ce6b9c4
truelifedev <truelife1228@outlook.com>
2022-03-11 00:37:45 -0500

    fix navbar

--- src/layouts/dashboard/navbar/NavbarDocs.js
+++ src/layouts/dashboard/navbar/NavbarDocs.js
@@ -17,20 +17,8 @@ export default function NavbarDocs() {
       spacing={3}
       sx={{ px: 5, pb: 5, mt: 10, width: 1, textAlign: 'center', display: 'block' }}
     >
-      <DocIllustration sx={{ width: 1 }} />
-
-      <div>
-        <Typography gutterBottom variant="subtitle1">
-          Hi, {user?.displayName}
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Need help?
-          <br /> Please check our docs
-        </Typography>
-      </div>
-
       <Button href={PATH_DOCS} target="_blank" rel="noopener" variant="contained">
-        Documentation
+        Discord Chat
       </Button>
     </Stack>
   );
