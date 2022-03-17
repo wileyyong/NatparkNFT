910f8e4186d9bd2f25c8dc42a0fd87997f888a61
truelifedev <truelife1228@outlook.com>
2022-03-17 17:01:08 +0700

    fixed leaderboard page count match issue

--- src/pages/LeaderBoard.js
+++ src/pages/LeaderBoard.js
@@ -2,11 +2,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { useMoralisWeb3Api } from "react-moralis";
+import axios from 'axios';
 import { ADMIN_WALLET } from 'src/config'; 
 import {
 	TableContainer, Table, TableHead, TableRow, 
 	TableCell, TableBody, Paper, TablePagination, TableFooter,
-	Link,
+	Link, CircularProgress, Box
 } from '@mui/material';
 
 export default function LeaderBoard() {
@@ -18,9 +19,39 @@ export default function LeaderBoard() {
     const [users, setUsers] = useState([]);
 		const [page, setPage] = useState(0);
 		const [rowsPerPage, setRowsPerPage] = useState(10);
+		const [total, setTotal] = useState(0);
+		const [loading, setLoading] = useState(false);
 
-		// const emptyRows =
-		// 	page > 0 ? Math.max(0, (1 + page) * rowsPerPage - users.length) : 0;
+		useEffect(async() => {
+			await fetchAll();
+		}, [nfts, setNFTs]);
+
+		useEffect(() => {
+			users.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(async(user) => {
+				if (!user.parks) {
+					const promises = user.nfts.map((item) => new Promise( resolve => {
+						getRealData(item.token_address, item.token_id).then(value => {
+							resolve({
+								...item,
+								data: value,
+							});
+						}).catch((e) => resolve(e));
+					}));
+					const list = await Promise.all(promises);
+					const result = [];
+					await list.forEach(item => {
+						if(item.data && item.data.attributes) {
+							const trait = item.data.attributes.find(attr => attr.trait_type === 'Parks');
+							if(trait && !result.includes(trait.value)) {
+								result.push(trait.value);
+							}
+						}
+					});
+					user.parks = result.length;
+					setUsers([...users]);
+				}
+			});
+		}, [page, users.length]);
 
 		const handleChangePage = (event, newPage) => {
 			setPage(newPage);
@@ -31,14 +62,27 @@ export default function LeaderBoard() {
 			setPage(0);
 		};
 
-    useEffect(() => {
+		const fetchAll = () => {
 			const options = {
 				address: ADMIN_WALLET,
+				offset: nfts.length,
 			};
-			getNFTOwners(options).then((data) => {
-				setNFTs(data.result);
+			setLoading(true);
+			getNFTOwners(options).then(async (data) => {
+				if(data?.result?.length) {
+					setNFTs(prev => [
+						...prev, 
+						...data.result
+					]);
+					setTotal(data.total);
+				}
 			});
-    }, []);
+		};
+
+		const getRealData = async (address, tokenId) => {
+			const { data } = await axios.get(`/contracts/${address}/${tokenId}`);
+			return data;
+		}
 
     useEffect(() => {
 			const uObj = {};
@@ -46,22 +90,26 @@ export default function LeaderBoard() {
 				if(uObj[item.owner_of]) {
 					uObj[item.owner_of] = {
 						...uObj[item.owner_of],
-						items: uObj[item.owner_of].items + 1
+						items: uObj[item.owner_of].items + 1,
+						nfts: [...uObj[item.owner_of].nfts, item]
 					};
 				} else {
 					uObj[item.owner_of] = {
 						address: item.owner_of,
-						items: 1
+						items: 1,
+						nfts: [item]
 					};
 				}
 			});
-			setUsers(
-				Object.keys(uObj)
-				.map(key => uObj[key])
-				.sort((a, b) => b.items - a.items)
-			);
+			if (nfts.length === total) {
+				setUsers(
+					Object.keys(uObj)
+					.map(key => uObj[key])
+					.sort((a, b) => b.items - a.items)
+				);
+				setLoading(false);
+			}
     }, [nfts]);
-
 
     return (
 			<div>
@@ -77,28 +125,38 @@ export default function LeaderBoard() {
 						</TableHead>
 						<TableBody>
 							{
-								(rowsPerPage > 0
-									? users.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-									: users
-								).map((row) => (
-									<TableRow
-										key={row.address}
-										sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-									>
-										<TableCell component="th" scope="row">
-											<Link href={`/owners/${row.address}`}>{row.address}</Link>
+								loading ? <TableRow>
+										<TableCell component="th" scope="row" colSpan={4}>
+											<Box display="flex" justifyContent='center' alignItems='center' height="50vh">
+												<CircularProgress color="success" />
+											</Box>
 										</TableCell>
-										<TableCell>{row.address}</TableCell>
-										<TableCell>{row.items}</TableCell>
-										<TableCell>{row.items}</TableCell>
-									</TableRow>
-							))}
+									</TableRow> : <>
+									{
+										(rowsPerPage > 0
+											? users.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+											: users
+										).map((row) => (
+											<TableRow
+												key={row.address}
+												sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+											>
+												<TableCell component="th" scope="row">
+													<Link href={`/owners/${row.address}`}>{row.address}</Link>
+												</TableCell>
+												<TableCell>{row.address}</TableCell>
+												<TableCell>{row.items}</TableCell>
+												<TableCell>{row.parks || <CircularProgress size={20} color="success" />}</TableCell>
+											</TableRow>
+									))}
+								</>
+							}
 						</TableBody>
 						<TableFooter>
           <TableRow>
             <TablePagination
               rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              colSpan={3}
+              colSpan={4}
               count={users.length}
               rowsPerPage={rowsPerPage}
               page={page}
